554d9ea6e8f262f7e7f3eb761b29e7bd159c35b4
PatriziaCrimi <crimi.patrizia@gmail.com>
2021-01-05 21:54:19 +0100

    All data organized in separate files ("include" added to index.php) & transform:rotate-scaleX added to language icon

---
+++ content.php
@@ -0,0 +1,113 @@
+<?php
+  $title = 'Domande frequenti';
+  $section_details = [
+    'title' => 'Privacy e termini',
+    'url' => '#',
+  ];
+  $nav_menu_links = [
+    [
+      'link' => 'Introduzione',
+      'url' => '#',
+    ],
+    [
+      'link' => 'Norme sulla privacy',
+      'url' => '#',
+    ],
+    [
+      'link' => 'Termini di servizio',
+      'url' => '#',
+    ],
+    [
+      'link' => 'Tecnologie',
+      'url' => '#',
+    ],
+    [
+      'link' => 'Domande frequenti',
+      'url' => '#',
+    ],
+  ];
+  $footer_links = [
+    [
+      'name' => 'Google',
+      'url' => '#',
+    ],
+    [
+      'name' => 'Tutto su Google',
+      'url' => '#',
+    ],
+    [
+      'name' => 'Privacy',
+      'url' => '#',
+    ],
+    [
+      'name' => 'Termini',
+      'url' => '#',
+    ],
+  ];  // Closing footer links list
+  $languages_list = [
+    [
+      'language' => 'Afrikaans',
+      'code' => 'af',
+    ],
+    [
+      'language' => 'Bahasa Indonesia',
+      'code' => 'id',
+    ],
+    [
+      'language' => 'Bahasa Melayu',
+      'code' => 'ms',
+    ],
+    [
+      'language' => 'Català',
+      'code' => 'ca',
+    ],
+    [
+      'language' => 'Čeština',
+      'code' => 'cs',
+    ],
+    [
+      'language' => 'Dansk',
+      'code' => 'da',
+    ],
+    [
+      'language' => 'Deutsch',
+      'code' => 'de',
+    ],
+    [
+      'language' => 'Eesti',
+      'code' => 'et',
+    ],
+    [
+      'language' => 'English',
+      'code' => 'en',
+    ],
+    [
+      'language' => 'English (United Kingdom)',
+      'code' => 'en-GB',
+    ],
+    [
+      'language' => 'Español',
+      'code' => 'es',
+    ],
+    [
+      'language' => 'Italiano',
+      'code' => 'it',
+    ],
+    [
+      'language' => 'Kiswahili',
+      'code' => 'sw',
+    ],
+    [
+      'language' => 'Latviešu',
+      'code' => 'lv',
+    ],
+    [
+      'language' => 'Lietuvių',
+      'code' => 'lt',
+    ],
+    [
+      'language' => 'Magyar',
+      'code' => 'hu',
+    ],
+  ];  // Closing languages list
+?>
